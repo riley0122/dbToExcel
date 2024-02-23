32ef56bf17750e4070af1364bb2fce938713ae47
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-23 17:56:21 +0100

    Initial commit

---
+++ src/main/java/dev/riley0122/dbToExcel/App.java
@@ -0,0 +1,13 @@
+package dev.riley0122.dbToExcel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/dev/riley0122/dbToExcel/AppTest.java
@@ -0,0 +1,20 @@
+package dev.riley0122.dbToExcel;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
