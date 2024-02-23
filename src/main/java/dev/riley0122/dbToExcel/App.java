17c3eb301e155d0ff5c500a89dd947692025bdfb
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-23 18:34:57 +0100

    Added parsing arguements

--- src/main/java/dev/riley0122/dbToExcel/App.java
+++ src/main/java/dev/riley0122/dbToExcel/App.java
@@ -1,4 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package dev.riley0122.dbToExcel;
+
+import java.util.Properties;
 
 /**
  * Hello world!
@@ -6,8 +13,30 @@ package dev.riley0122.dbToExcel;
  */
 public class App 
 {
+    public static Properties parseArgs(String[] args)
+    {
+        Properties arguements = new Properties();
+        for (String arguement : args)
+        {
+            String arg = arguement.substring(2);
+
+            if (arg.contains("="))
+            {
+                String[] split = arg.split("=");
+                arguements.setProperty(split[0], split[1]);
+            } else
+            {
+                arguements.setProperty(arg, "true");
+            }
+        }
+
+        return arguements;
+    }
+
     public static void main( String[] args )
     {
+        Properties parsedArgs = parseArgs(args);
+        
         System.out.println( "Hello World!" );
     }
 }
--- src/test/java/dev/riley0122/dbToExcel/AppTest.java
+++ src/test/java/dev/riley0122/dbToExcel/AppTest.java
@@ -1,20 +1,40 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package dev.riley0122.dbToExcel;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.Properties;
 
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit tests
  */
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
+     * Testing parsing arguements
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void EmptyArgsGivesEmptyResult()
     {
-        assertTrue( true );
+        Properties args = App.parseArgs(new String[0]);
+        assertEquals(new Properties(), args);
+        assertNull(args.getProperty("something"));
+    }
+
+    @Test
+    public void CheckValuesAreBeingSet()
+    {
+        String[] args = {"--key=value", "--verbose", "--something=nothing"};
+        Properties parsedArgs = App.parseArgs(args);
+        assertEquals("value", parsedArgs.getProperty("key"));
+        assertEquals("true", parsedArgs.getProperty("verbose"));
+        assertEquals("nothing", parsedArgs.getProperty("something"));
     }
 }
